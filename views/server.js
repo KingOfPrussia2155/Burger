1ba4961252553dab1dce100b8e2b89fdb0ee52b8
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-09-15 16:33:43 -0600

    set var

--- views/server.js
+++ views/server.js
@@ -1,3 +1,12 @@
 var express = require ('express');
 var bodyParser = require('body-parser');
 var methodOverride =require('method-override');
+var handleBars = require('express-handlebars');
+var app = express();
+app.use(express.static(_direname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
