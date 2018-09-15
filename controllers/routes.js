89ce5f525f9edeb603b8a06458a76004ed19b351
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-09-15 17:20:52 -0600

    more changes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var burger = require('../models/burger.js');
 var router = express.Router();
 router.get('/', function(req,res){
-    res.render('index');
+    burger.all(function(burger_data){
+    console.log(burger_data);
+        res.render('index');
+    })
 })
+module.exports = router;
